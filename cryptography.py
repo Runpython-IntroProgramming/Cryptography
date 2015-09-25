b2830744ca313f1e26278c1ca74be74638ec410c
Eric Dennison <ericd@netdenizen.com>
2015-09-25 11:50:50 -0400

    Update cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -9,3 +9,4 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
